9a310014c09f7339fbbf6e9e16bcf5d7be1041ca
Glitch (glitch-hello-website) <none>
2022-05-29 16:01:53 +0000

    🤘💨 Checkpoint ./index.js:45080849/3238

--- index.js
+++ index.js
@@ -49,20 +49,7 @@ class Ripple {
     this.button = button;
     this.rippleFadeDelay = 600;
 
-    button.addEventListener("mousedown", (e) => {
-      this.rippleEnter(e);
-    });
-    
-    button.addEventListener("mouseup", (e) => {
-      this.rippleLeave(e);
-    });
-  }
-
-  calcDim() {
-    return this.button.getBoundingClientRect();
-  }
-  
-  rippleEnter(e) {
+    button.addEventListener("click", (e) => {
       this.dim = this.calcDim();
       this.circle = document.createElement("span");
       this.diameter = Math.max(
@@ -84,20 +71,23 @@ class Ripple {
       this.circle.style.top = this.y;
       this.circle.classList.add("_ripple");
       this.button.appendChild(this.circle);
+      
+      this.removeRipples();
+    });
   }
-  
-  rippleLeave() {
-    this.removeRipples();
+
+  calcDim() {
+    return this.button.getBoundingClientRect();
   }
 
   removeRipples() {
-    let ripple = this.button.querySelector("._ripple");
+    let ripples = this.button.querySelectorAll("._ripple");
     
     setTimeout(function() {
-      
-      
-      if (ripple) {
-        ripple.remove();
+      this._ripple = ripples;
+
+      if (this._ripple) {
+        this._ripple.remove();
       }
     }, this.rippleFadeDelay);
   }
