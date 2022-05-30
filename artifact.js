4295ce74776989b88a8ddd2c7f0aa09f59fc3df2
Glitch (glitch-hello-website) <none>
2022-05-30 23:28:02 +0000

    🎺💸 Checkpoint ./artifact.js:45080849/269 ./style.css:45080849/458 ./index.html:45080849/29

--- artifact.js
+++ artifact.js
@@ -2,15 +2,17 @@ $("#app").html(`<div class="loader"></div>`);
 
 $.ajax({
   type: "GET",
-  url: "/artifact-1.html",
+  url: "/artifact/artifact-1.html",
   dataType: "html",
   error: function(xhr, status, error) {
     $("#app").html(`
       <div class="alert alert-danger">
-        <div class="">
-          <i class="fa-solid fa-triangle-exclamation"></i> An Error Occured: ${xhr.responseText}
-          
-        
+        <div class="alert-icon">
+          <i class="fa-solid fa-triangle-exclamation"></i>
+        </div>
+        <div class="alert-message">
+          An Error Occured: ${xhr.responseText}
+        </div>
       </div>
     `);
   },
