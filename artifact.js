617e37a64b7c999062f3359e4a0fe339762b1e75
Glitch (glitch-hello-website) <none>
2022-05-30 22:05:55 +0000

    👑💾 Checkpoint ./artifact-1.html:45080849/19 ./artifact.js:45080849/684 ./style.css:45080849/1136

--- artifact.js
+++ artifact.js
@@ -1,20 +1,20 @@
 $("#app").html(`<div class="loader"></div>`);
 
 $.ajax({
-  type: 'GET',
+  type: "GET",
   url: "/artifact-1.html",
-  dataType: 'html',
-  success: function (res){
-     $("#app").html(res);
-  }
+  dataType: "html",
+  success: function (res) {
+    $("#app").html(res);
+  },
 });
 
-if(window.location.hash) {
-  console.log("hello")
+if (window.location.hash) {
+  console.log("hello");
 } else {
   console.log("nothing");
 }
 
-addEventListener('hashchange', function(e) {
-  console.log("hello")
+addEventListener("hashchange", function (e) {
+  console.log("hello");
 });
