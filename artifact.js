afd21f5e9147baf43bc463cf5eac654f4b9deef8
Glitch (glitch-hello-website) <none>
2022-05-30 21:18:49 +0000

    ⛪️🏡 Checkpoint ./index.html:45080849/306 ./index.js:45080849/2 ./artifact.js:45080849/1164 ./text.js:45080849/27 ./text.txt:45080849/1

--- artifact.js
+++ artifact.js
@@ -1,3 +1,16 @@
+$("#app").html(`<div class="loader"></div>`);
+
+$.ajax({
+  type: 'GET',
+  url: "http://localhost:3000/UserRegistration",
+  dataType: 'txt',
+  success: function (response){
+     //supposing you have an html element where you want to append 
+     //the response, with an id like appendResponse
+     $('#app').html(response);
+  }
+});
+
 if(window.location.hash) {
   console.log("hello")
 } else {
