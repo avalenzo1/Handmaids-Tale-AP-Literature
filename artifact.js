c7726cb10596768e8a9e7f428c6fb882faf49e8e
Glitch (glitch-hello-website) <none>
2022-06-03 13:38:59 +0000

    📻💿 Checkpoint ./artifact.js:45080849/3312 ./index.html:45080849/27

--- artifact.js
+++ artifact.js
@@ -1,24 +1,31 @@
 $("#app").hide();
 
-$(function(){
+$(function () {
   // artifact
-  
+
   function redirectPage() {
     if (window.location.hash) {
-      $("#heading-main").hide();
-      $("#app").show();
-      $("#app").html(`<div style="text-align: center;"><div class="loader"></div></div>`);
-      
       if (window.location.hash.match(/#!\//i)) {
         let hashRegexPath = window.location.hash.replace("#!/", "");
-        let urlRedirect = new URL('https://handmaids-tale-project.glitch.me/' + hashRegexPath);
 
-        $.ajax({
-          type: "GET",
-          url: urlRedirect,
-          dataType: "html",
-          error: function(xhr, status, error) {
-            $("#app").html(`
+        if (hashRegexPath !== "") {
+          $("#heading-main").hide();
+          $("#app").show();
+          $("#app").html(
+            `<div style="text-align: center;"><div class="loader"></div></div>`
+          );
+
+          
+          let urlRedirect = new URL(
+            "https://handmaids-tale-project.glitch.me/artifact/" + hashRegexPath
+          );
+
+          $.ajax({
+            type: "GET",
+            url: urlRedirect,
+            dataType: "html",
+            error: function (xhr, status, error) {
+              $("#app").html(`
               <div class="alert alert-warning">
                 <div class="alert-icon">
                   <i class="fa-solid fa-triangle-exclamation"></i>
@@ -28,18 +35,19 @@ $(function(){
                 </div>
               </div>
             `);
-          },
-          success: function (response) {
-            $("#app").html(response);
-          },
-        }); 
+            },
+            success: function (response) {
+              $("#app").html(response);
+            },
+          });
+        }
       }
     }
   }
 
   redirectPage();
-  
-  window.addEventListener("hashchange", function() {
-    redirectPage()
+
+  window.addEventListener("hashchange", function () {
+    redirectPage();
   });
 });
